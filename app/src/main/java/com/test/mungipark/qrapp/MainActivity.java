236b4c0804614ddd88da53e560db848b7bfc152e
MungiPark <prgbmk@naver.com>
2015-05-31 19:44:13 +0900

    <Implemented> Custom View, and Push Client Components

--- app/src/main/java/com/test/mungipark/qrapp/MainActivity.java
+++ app/src/main/java/com/test/mungipark/qrapp/MainActivity.java
@@ -1,16 +1,24 @@
 package com.test.mungipark.qrapp;
 
+import android.app.ActionBar;
 import android.app.Activity;
+import android.content.Context;
 import android.os.AsyncTask;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.content.Intent;
 import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
 import android.widget.Button;
+import android.widget.ListView;
 import android.widget.TextView;
 import android.widget.Toast;
+
+import com.google.android.gms.gcm.GoogleCloudMessaging;
 
 import java.io.BufferedReader;
 import java.io.IOException;
@@ -20,32 +28,69 @@ import java.io.PrintWriter;
 import java.net.HttpURLConnection;
 import java.net.MalformedURLException;
 import java.net.URL;
+import java.util.ArrayList;
 
 
 public class MainActivity extends Activity {
 
-    private TextView tvResult, QRdateText, QRnameText, QRnumberText, QRtypeText;
-    private String result;//DB결과값
+    //UI들
+    private TextView tvResult;
 
+    //GCM 받기 위한 객체들
+    private String SENDER_ID = "1077167000598";//프로젝트 API 생성 아이디.
+    private GoogleCloudMessaging gcm;
+    private String regid;
 
     //QR 코드에서 넘어오는 값(처방내용)을 처리해주기 위한 String 객체 - DB 내용추가 참고용
     private String QRdate, QRname[], QRnumber[], QRtype[];
     private int i;
 
+    //커스텀된 ArrayList
+    ArrayList<MydescriptionValue> descriptionValues;
+    MydescriptionValue myvalue;//센서값 담는 객체(커스텀된거) - 그림, 값.
+    ListView descriptionValuesList;
+
+    //어댑터 준비
+    MydescriptionValueAdapter Adapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+
+        //GCM용 객체 초기화
+        gcm = GoogleCloudMessaging.getInstance(this);
+        registerInBackground();
+
+        //커스텀 리스트뷰용 객체 선언.
+        descriptionValues = new ArrayList<MydescriptionValue>();
+        Adapter = new MydescriptionValueAdapter(this, R.layout.item, descriptionValues);
+
         setUpView();
     }
 
+    //어댑터 리스트에 DB 내용 간추려 넣는 메소드
+    private void setAdapter(String DB_Result){
+        String enterTok[] = null;
+        enterTok = DB_Result.split("\n");
 
+        int i=1;
+        while(i <enterTok.length){
+            String starTok[] = enterTok[i].split("[*]");
+            myvalue = new MydescriptionValue(starTok[0], starTok[1] + " " + starTok[2] + "개 남음");
+            descriptionValues.add(myvalue);
+            i++;
+        }
+        //모아진 어댑터 값을 ListView에 적용
+        descriptionValuesList.setAdapter(Adapter);
+    }
+
+    //onCreate()메소드에 실을 초기화 메소드
     private void setUpView() {
         // TODO Auto-generated method stub
         tvResult = (TextView) this.findViewById(R.id.textViewResult);
-
+        descriptionValuesList = (ListView)this.findViewById(R.id.descriptionListView);
 
         Button btnScanQRCode = (Button) this
                 .findViewById(R.id.buttonScanQrCode);
@@ -68,7 +113,7 @@ public class MainActivity extends Activity {
             @Override
             public void onClick(View v) {
                 new showDB().execute();
-                tvResult.setText(result);
+                //tvResult.setText(result);
             }
         });
 
@@ -100,7 +145,7 @@ public class MainActivity extends Activity {
 
 
             Log.d("name : ", QRname[i]);
-            new inputDB().execute();//DB접근 스레드 시작.
+            new inputDB().execute();//DB삽입 스레드 시작.
 
 
             tvResult.setText(contantsString);
@@ -135,7 +180,6 @@ public class MainActivity extends Activity {
 
         protected void onPostExecute(String temp){
 
-
         }
     }
 
@@ -147,6 +191,14 @@ public class MainActivity extends Activity {
             Log.d("Show DB스레드 작업 : ", "실행완료");
             output = ShowData();
             return output;
+        }
+
+        @Override
+        protected void onPostExecute(String s) {
+            super.onPostExecute(s);
+            //tvResult.setText(s);
+            //커스텀 리스트뷰에 DB 결과를 받아와 저장함.
+            setAdapter(s);
         }
     }
 
@@ -163,7 +215,7 @@ public class MainActivity extends Activity {
         // Handle action bar item clicks here. The action bar will
         // automatically handle clicks on the Home/Up button, so long
         // as you specify a parent activity in AndroidManifest.xml.
-      int id = item.getItemId();
+        int id = item.getItemId();
 
         //noinspection SimplifiableIfStatement
         if (id == R.id.action_settings) {
@@ -175,11 +227,12 @@ public class MainActivity extends Activity {
 
     //DB데이터 쓰는 함수 - AsyncTask 스레드에 탐재할 함수
     private String InsertData(){
+        String result = null;
         URL url = null;
         try {
 
             while(i<QRname.length) {
-                url = new URL("http://192.168.0.104/insert_menu.php");
+                url = new URL("http://119.199.154.131/insert_menu(Description).php");
 
                 HttpURLConnection http = (HttpURLConnection) url.openConnection();//php접속
 
@@ -237,10 +290,11 @@ public class MainActivity extends Activity {
     }
     //DB데이터 읽는 함수(해당 날짜의 데이터만 읽어오기) - AsyncTask 스레드에 탐재할 함수
     private String ShowData(){
+        String result = null;//DB결과 받아서 반환할 객체
         URL url = null;
         try {
             //url = new URL("http://192.168.0.104/show_data_date.php");
-            url = new URL("http://192.168.0.104/show_data.php");
+            url = new URL("http://119.199.154.131/show_data.php");
 
             HttpURLConnection http = (HttpURLConnection) url.openConnection();//php접속
 
@@ -252,7 +306,7 @@ public class MainActivity extends Activity {
 
             //php에 파라미터 넘겨주는 작업 시작.
             StringBuffer buffer = new StringBuffer();
-            buffer.append("date").append("=").append("20150503");
+            //buffer.append("date").append("=").append("20150504");
 
 
             //Php에 파라미터 값 넘기기
@@ -269,6 +323,7 @@ public class MainActivity extends Activity {
             while((str = reader.readLine()) != null){
                 builder.append(str + "\n");
             }
+
             result = builder.toString();
 
             Log.d("Show_Data.php :", result);
@@ -284,5 +339,91 @@ public class MainActivity extends Activity {
         return result;
     }
 
+    //GCM쓰기위한 메소드 부분들.
+    private void registerInBackground() {
+        new AsyncTask<Void, Void, String>() {
+            @Override
+            protected String doInBackground(Void... params) {
+                try {
+                    regid = gcm.register(SENDER_ID);
+                    sendRegistrationIdToBackend();
+                } catch (IOException ex) {
+                }
+                return "";
+            }
+
+            @Override
+            protected void onPostExecute(String msg) {
+            }
+        }.execute(null, null, null);
+    }
+
+    private void sendRegistrationIdToBackend() {
+        // Your implementation here.
+
+        Log.d(null, "RegId = " + regid);
+    }
+
+}
+
+//리스트뷰 출력 항목 클래스
+class MydescriptionValue {
+    String Date;
+    String Description;
+
+    //기본 생성자.
+    MydescriptionValue(String Date, String Description){
+        this.Date = Date;
+        this.Description = Description;
+    }
+}
+
+//BaseAdapter 인터페이스를 오버라이드하여 커스텀 구현
+class MydescriptionValueAdapter extends BaseAdapter {
+
+    Context con;
+    LayoutInflater inflater;
+    ArrayList<MydescriptionValue> arD;
+    int layout;
+
+    //기본 생성자
+    public MydescriptionValueAdapter(Context con, int layout, ArrayList<MydescriptionValue> arD){
+        this.con = con;
+        this.inflater = (LayoutInflater) con.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+        this.arD = arD;
+        this.layout = layout;
+    }
+
+    //어댑터 몇 개의 항목이 있는지 확인
+    @Override
+    public int getCount() {
+        return arD.size();
+    }
+
+    //Position 위치의 항목 Value 반환
+    @Override
+    public Object getItem(int position) {
+        return arD.get(position).Date;
+    }
+
+    //Position 위치의 ID 반환
+    @Override
+    public long getItemId(int position) {
+        return position;
+    }
+
+    @Override
+    public View getView(final int position, View convertView, ViewGroup parent) {
+        if(convertView == null){
+            convertView = inflater.inflate(layout, parent, false);
+        }
+        TextView date = (TextView) convertView.findViewById(R.id.date);
+        date.setText(arD.get(position).Date);
+
+        TextView description = (TextView) convertView.findViewById(R.id.description);
+        description.setText(arD.get(position).Description);
+
+        return convertView;
+    }
 }
 
